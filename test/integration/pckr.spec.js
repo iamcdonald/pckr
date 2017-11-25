97f2d2e203279b80517ac0a83ff9b560feba800a
Iain McDonald <iain.allan.mcdonald@gmail.com>
2017-11-25 17:06:56 +0000

    fix integration tests and reporting

--- test/integration/pckr.spec.js
+++ test/integration/pckr.spec.js
@@ -5,5 +5,6 @@ const pckr = require('../../src');
 
 test('pack - packs given module and returns path to .tgz', async t => {
   const packedPath = await pckr.pack(path.resolve(__dirname, 'test-project'));
-  t.truthy(fs.existsSync(packedPath));
+  const packageExists = fs.existsSync(packedPath);
+  t.truthy(packageExists);
 });
